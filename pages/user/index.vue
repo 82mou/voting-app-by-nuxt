25ea9be082fe951913b62efafa0d3a26b1abe11c
kazuya hatsushi <kazuya_hatsushi@dwango.co.jp>
2021-03-20 09:48:18 +0900

    update: workspaceに保存時の整形追加 & 全コメント取得

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -3,7 +3,7 @@
     <div class="wrapper wrapper--user">
       <header class="l-header">
         <div class="l-container">
-          <h1 class="logo"><img src="~assets/images/logo.png" alt=""></h1>
+          <h1 class="logo"><img src="~assets/images/logo.png" alt="" /></h1>
         </div>
       </header>
       <main class="l-main">
@@ -53,10 +53,23 @@
       </main>
       <div class="form">
         <div class="l-container">
-          <form name="comment-form" id="js-form">
-            <input type="text" name="comment" maxlength="30" class="form__input js-comment" placeholder="コメントを入力してください">
+          <form
+            name="comment-form"
+            id="js-form"
+            @submit.prevent="onSubmitComment"
+          >
+            <input
+              v-model="commentText"
+              type="text"
+              name="comment"
+              maxlength="30"
+              class="form__input js-comment"
+              placeholder="コメントを入力してください"
+            />
             <div class="btn-wrap">
-              <button type="submit" name="btn" class="btn" ref="submit">送信</button>
+              <button type="submit" name="btn" class="btn" ref="submit">
+                送信
+              </button>
             </div>
           </form>
         </div>
@@ -66,20 +79,24 @@
       <p class="view-pc__text">スマホ画面で表示して下さい</p>
     </div>
     <div class="layer js-layer dn">
-      <img class="layer__loading js-layer-loading dn" src="~assets/images/loading.gif" alt="">
+      <img
+        class="layer__loading js-layer-loading dn"
+        src="~assets/images/loading.gif"
+        alt=""
+      />
       <p class="layer__count-stop js-layer-count-stop dn">集計停止中</p>
     </div>
   </div>
 </template>
 
 <script>
-import firebase from '@/plugins/firebase';
+import firebase from "@/plugins/firebase";
 
 export default {
   data() {
     return {
-      TRANSITION_END: 'transitionend',// 2つ指定していると2回バインドされる
-      ANIMATION_END: 'animationend',// 2つ指定していると2回バインドされる
+      TRANSITION_END: "transitionend", // 2つ指定していると2回バインドされる
+      ANIMATION_END: "animationend", // 2つ指定していると2回バインドされる
 
       /*
        * 初期化
@@ -100,33 +117,38 @@ export default {
       countObj: {},
       commentObj: {},
       counts: {
-        a: '',
-        b: '',
-        c: '',
-        d: '',
+        a: "",
+        b: "",
+        c: "",
+        d: "",
       },
       countStop: false,
       layer: {},
       layerLoading: {},
-      layerCountStop: {}
-    }
+      layerCountStop: {},
+      commentText: "",
+    };
   },
   mounted() {
     this.database = firebase.firestore();
-    this.refTitle = this.database.collection('titles').doc('title');
-    this.refCount = this.database.collection('counts').doc('count');
-    this.refName = this.database.collection('names').doc('name');
-    this.refComment = this.database.collection('pushComments').doc('pushComment');
-    this.refCountStop = this.database.collection('counterSwitcher').doc('counterSwitch');
-    this.refView = this.database.collection('viewCounterSwitcher').doc('viewCounterSwitch');
+    this.refTitle = this.database.collection("titles").doc("title");
+    this.refCount = this.database.collection("counts").doc("count");
+    this.refName = this.database.collection("names").doc("name");
+    this.refComment = this.database.collection("pushComments");
+    this.refCountStop = this.database
+      .collection("counterSwitcher")
+      .doc("counterSwitch");
+    this.refView = this.database
+      .collection("viewCounterSwitcher")
+      .doc("viewCounterSwitch");
     this.title = this.$refs.title;
-    this.post = document.querySelectorAll('.js-post');
+    this.post = document.querySelectorAll(".js-post");
     // this.submit = this.$refs.submit;
-    this.form = document.querySelector('#js-form');
-    this.count = document.querySelectorAll('.js-count');
-    this.layer = document.querySelector('.js-layer');
-    this.layerLoading = document.querySelector('.js-layer-loading');
-    this.layerCountStop = document.querySelector('.js-layer-count-stop');
+    this.form = document.querySelector("#js-form");
+    this.count = document.querySelectorAll(".js-count");
+    this.layer = document.querySelector(".js-layer");
+    this.layerLoading = document.querySelector(".js-layer-loading");
+    this.layerCountStop = document.querySelector(".js-layer-count-stop");
     /*
      * カウント表示/非表示判定
      */
@@ -145,39 +167,40 @@ export default {
 
     for (let i = 0; i < this.post.length; i++) {
       let el = this.post[i];
-      el.addEventListener('click', () => {
+      el.addEventListener("click", () => {
         if (this.countStop) {
           return;
         }
-        let initial = el.dataset['initial'];
+        let initial = el.dataset["initial"];
         this.countObj[initial] = this.countObj[initial] + 1;
         this.postActionCount(initial, this.countObj[initial]);
       });
     }
 
-    this.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      let commentVal = this.form.querySelector('.js-comment').value;
+    // Nuxt移行完了までコメントアウト
+    // this.form.addEventListener('submit', (e) => {
+    //   e.preventDefault();
+    //   let commentVal = this.form.querySelector('.js-comment').value;
 
-      this.postActionComment(commentVal);
+    //   this.postActionComment(commentVal);
 
-      // データベースに送信後は値を空にする
-      this.form.querySelectorAll('.js-comment').value;
-    });
+    //   // データベースに送信後は値を空にする
+    //   this.form.querySelectorAll('.js-comment').value;
+    // });
 
-    this.refTitle.on("child_added", (snapshot) => {
-      // データベースと同期
-      //titleObj[snapshot.key] = snapshot.val();
-      //renderTitle(titleObj);
-      this.defRenderTitle({
-        id: snapshot.key,
-        value: snapshot.val()
-      });
-    });
+    // this.refTitle.on("child_added", (snapshot) => {
+    //   // データベースと同期
+    //   //titleObj[snapshot.key] = snapshot.val();
+    //   //renderTitle(titleObj);
+    //   this.defRenderTitle({
+    //     id: snapshot.key,
+    //     value: snapshot.val()
+    //   });
+    // });
 
-    this.refTitle.on("value", (snapshot) => {
-      this.renderTitle(snapshot.val());
-    });
+    // this.refTitle.on("value", (snapshot) => {
+    //   this.renderTitle(snapshot.val());
+    // });
 
     //refName.on("child_added", (snapshot) => {
     //  // データベースと同期
@@ -185,89 +208,89 @@ export default {
     //  renderName(nameObj);
     //});
 
-    this.refName.on("child_added", (snapshot) => {
-      // データベースと同期
-      //nameObj[snapshot.key] = snapshot.val();
+    // this.refName.on("child_added", (snapshot) => {
+    //   // データベースと同期
+    //   //nameObj[snapshot.key] = snapshot.val();
 
-      this.defRenderName({
-        id: snapshot.key,
-        value: snapshot.val()
-      });
-    });
+    //   this.defRenderName({
+    //     id: snapshot.key,
+    //     value: snapshot.val()
+    //   });
+    // });
 
-    this.refName.on("value", (snapshot) => {
-      this.renderName(snapshot.val());
-    });
+    // this.refName.on("value", (snapshot) => {
+    //   this.renderName(snapshot.val());
+    // });
 
-    this.refCount.on("child_added", (snapshot) => {
-      // データベースと同期
-      this.countObj[snapshot.key] = snapshot.val();
+    // this.refCount.on("child_added", (snapshot) => {
+    //   // データベースと同期
+    //   this.countObj[snapshot.key] = snapshot.val();
 
-      this.defRenderCount({
-        id: snapshot.key,
-        value: snapshot.val()
-      });
-    });
+    //   this.defRenderCount({
+    //     id: snapshot.key,
+    //     value: snapshot.val()
+    //   });
+    // });
 
-    this.refCount.on("value", (snapshot) => {
-      let snapshotObj = snapshot.val();
+    // this.refCount.on("value", (snapshot) => {
+    //   let snapshotObj = snapshot.val();
 
-      // データベースと同期
-      for (let key in snapshotObj){
-        this.countObj[key] = snapshotObj[key];
-      }
+    //   // データベースと同期
+    //   for (let key in snapshotObj){
+    //     this.countObj[key] = snapshotObj[key];
+    //   }
 
-      this.renderCount(snapshot.val());
-    });
+    //   this.renderCount(snapshot.val());
+    // });
 
-    this.refView.on("child_added", (snapshot) => {
-      if(snapshot.val() === true) {
-        for (let i = 0; i < this.count.length; i++) {
-          this.count[i].classList.remove('is-hide');
-        }
-      } else {
-        for (let i = 0; i < this.count.length; i++) {
-          this.count[i].classList.add('is-hide');
-        }
-      }
-    });
+    // this.refView.on("child_added", (snapshot) => {
+    //   if(snapshot.val() === true) {
+    //     for (let i = 0; i < this.count.length; i++) {
+    //       this.count[i].classList.remove('is-hide');
+    //     }
+    //   } else {
+    //     for (let i = 0; i < this.count.length; i++) {
+    //       this.count[i].classList.add('is-hide');
+    //     }
+    //   }
+    // });
 
-    this.refView.on("value", (snapshot) => {
-      if(snapshot.val().view === true) {
-        for (let i = 0; i < this.count.length; i++) {
-          this.count[i].classList.remove('is-hide');
-        }
-      } else {
-        for (let i = 0; i < this.count.length; i++) {
-          this.count[i].classList.add('is-hide');
-        }
-      }
-    });
+    // this.refView.on("value", (snapshot) => {
+    //   if(snapshot.val().view === true) {
+    //     for (let i = 0; i < this.count.length; i++) {
+    //       this.count[i].classList.remove('is-hide');
+    //     }
+    //   } else {
+    //     for (let i = 0; i < this.count.length; i++) {
+    //       this.count[i].classList.add('is-hide');
+    //     }
+    //   }
+    // });
 
-    this.refCountStop.on("child_added", (snapshot) => {
-      if(snapshot.val() === true) {
-        this.countStop = true;
-        this.showCountStop();
-      } else {
-        this.countStop = false;
-        this.hideCountStop();
-      }
-    });
+    // this.refCountStop.on("child_added", (snapshot) => {
+    //   if(snapshot.val() === true) {
+    //     this.countStop = true;
+    //     this.showCountStop();
+    //   } else {
+    //     this.countStop = false;
+    //     this.hideCountStop();
+    //   }
+    // });
 
-    this.refCountStop.on("value", (snapshot) => {
-      if(snapshot.val().countStop === true) {
-        this.countStop = true;
-        this.showCountStop();
-      } else {
-        this.countStop = false;
-        this.hideCountStop();
-      }
-    });
+    // this.refCountStop.on("value", (snapshot) => {
+    //   if(snapshot.val().countStop === true) {
+    //     this.countStop = true;
+    //     this.showCountStop();
+    //   } else {
+    //     this.countStop = false;
+    //     this.hideCountStop();
+    //   }
+    // });
 
     for (let i = 0; i < this.post.length; i++) {
       let el = this.post[i];
       el.addEventListener(this.ANIMATION_END, () => {
-        el.classList.remove('is-animated');
+        el.classList.remove("is-animated");
       });
     }
   },
@@ -292,12 +315,12 @@ export default {
       this.checkFontSize($targetCountObj);
     },
     renderCount(countObj) {
-      for (let key in countObj){
-        if(parseInt($(`.js-count-${key}`).text(), 10) !== countObj[key]) {
+      for (let key in countObj) {
+        if (parseInt($(`.js-count-${key}`).text(), 10) !== countObj[key]) {
           let $targetCountObj = $(`.js-count-${key}`);
-          let $targetCountObjPost = $targetCountObj.closest('.js-post');
+          let $targetCountObjPost = $targetCountObj.closest(".js-post");
 
-          $targetCountObjPost.addClass('is-animated');
+          $targetCountObjPost.addClass("is-animated");
           $targetCountObj.text(countObj[key]);
           this.checkFontSize($targetCountObj);
         }
@@ -307,7 +330,7 @@ export default {
       $(`.js-name-${nameObj.id}`).text(nameObj.value);
     },
     renderName(nameObj) {
-      for (let key in nameObj){
+      for (let key in nameObj) {
         $(`.js-name-${key}`).text(nameObj[key]);
       }
     },
@@ -315,21 +338,22 @@ export default {
       let arg = {};
       arg[initial] = countVal;
       this.show();
-      this.refCount.update(arg)
-        .then((res)=>{
-          setTimeout(() => {
-            this.hide();
-          }, 300);
-        });
+      this.refCount.update(arg).then((res) => {
+        setTimeout(() => {
+          this.hide();
+        }, 300);
+      });
     },
-    postActionComment(commentVal) {
-      let arg = {};
-      arg['comment'] = commentVal;
-
-      this.refComment.push(arg)
-        .then((res)=>{
-          //console.log(res);
+    onSubmitComment() {
+      this.refComment.add({ text: this.commentText });
+      this.getData();
+    },
+    getData() {
+      this.refComment.get().then((snapshot) => {
+        snapshot.docs.forEach(function(doc) {
+          console.log(doc.data().text);
         });
+      });
     },
 
     /*
@@ -337,47 +361,46 @@ export default {
      */
     checkFontSize(targetCountObj) {
       let countTextLength = targetCountObj.text().length;
-      if(countTextLength > 3) {
-        targetCountObj.addClass('count__num--long');
+      if (countTextLength > 3) {
+        targetCountObj.addClass("count__num--long");
       } else {
-        targetCountObj.removeClass('count__num--long');
+        targetCountObj.removeClass("count__num--long");
       }
     },
     show() {
-      this.layer.classList.remove('dn');
-      this.layerLoading.classList.remove('dn');
-      this.layer.classList.add('is-show');
+      this.layer.classList.remove("dn");
+      this.layerLoading.classList.remove("dn");
+      this.layer.classList.add("is-show");
     },
     hide() {
-      this.layer.classList.remove('is-show');
+      this.layer.classList.remove("is-show");
 
       setTimeout(() => {
-        this.layerLoading.classList.add('dn');
-        this.layer.classList.add('dn');
+        this.layerLoading.classList.add("dn");
+        this.layer.classList.add("dn");
       }, 600);
     },
     showCountStop() {
-      this.layer.classList.remove('dn');
-      this.layerCountStop.classList.remove('dn');
-      this.layer.classList.add('is-show');
+      this.layer.classList.remove("dn");
+      this.layerCountStop.classList.remove("dn");
+      this.layer.classList.add("is-show");
     },
     hideCountStop() {
-      this.layer.classList.remove('is-show');
+      this.layer.classList.remove("is-show");
 
       setTimeout(() => {
-        this.layerCountStop.classList.add('dn');
-        this.layer.classList.add('dn');
+        this.layerCountStop.classList.add("dn");
+        this.layer.classList.add("dn");
       }, 600);
     },
     submit() {
       const db = firebase.firestore();
-      let counts = db.collection('counts');
-      let a = counts.doc('a');
-      a
-       .update({
-         count: 1000,
-       })
-    }
-  }
-}
+      let counts = db.collection("counts");
+      let a = counts.doc("a");
+      a.update({
+        count: 1000,
+      });
+    },
+  },
+};
 </script>
